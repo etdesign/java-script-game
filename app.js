c09f36fd54a2caa7868c8598d9cde82ac1054732
Emre Turan <itsemreturan@gmail.com>
2023-03-09 14:34:31 +0300

    updated logic and added error handler methods

--- app.js
+++ app.js
@@ -2,34 +2,50 @@ function computerPlay() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
-}
-
-function playRound(playerSelection, computerSelection) {
-    // Make playerSelection case-insensitive
+  }
+  
+  function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
   
-    if (playerSelection === computerSelection.toLowerCase()) {
-      return "It's a tie!";
-    } 
-    else if (
-      (playerSelection === "rock" && computerSelection.toLowerCase() === "scissors") ||
-      (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") ||
-      (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper")
-    ) 
-    {
-      return `You Win! ${playerSelection} beats ${computerSelection}.`;
-    } 
-    else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    switch (playerSelection) {
+      case "rock":
+        if (computerSelection.toLowerCase() === "scissors") {
+          return `You Win! ${playerSelection} beats ${computerSelection}.`;
+        } else if (computerSelection.toLowerCase() === "paper") {
+          return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        } else {
+          return "It's a tie!";
+        }
+      case "paper":
+        if (computerSelection.toLowerCase() === "rock") {
+          return `You Win! ${playerSelection} beats ${computerSelection}.`;
+        } else if (computerSelection.toLowerCase() === "scissors") {
+          return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        } else {
+          return "It's a tie!";
+        }
+      case "scissors":
+        if (computerSelection.toLowerCase() === "paper") {
+          return `You Win! ${playerSelection} beats ${computerSelection}.`;
+        } else if (computerSelection.toLowerCase() === "rock") {
+          return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        } else {
+          return "It's a tie!";
+        }
+      default:
+        return "Invalid selection. Please choose rock, paper, or scissors.";
     }
   }
   
-  function game() {
+  function game(numRounds) {
     let playerScore = 0;
     let computerScore = 0;
   
-    for (let i = 1; i <= 5; i++) {
-      const playerSelection = prompt(`Round ${i}: Choose Rock, Paper, or Scissors`);
+    for (let i = 1; i <= numRounds; i++) {
+      let playerSelection = prompt(`Round ${i}: Choose Rock, Paper, or Scissors`);
+      while (!["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
+        playerSelection = prompt(`Invalid selection. Please choose Rock, Paper, or Scissors`);
+      }
       const computerSelection = computerPlay();
       const result = playRound(playerSelection, computerSelection);
       console.log(result);
@@ -50,10 +66,16 @@ function playRound(playerSelection, computerSelection) {
     }
   }
   
-/*
-  console.log(playRound("rock", computerPlay())); 
-  console.log(playRound("paper", computerPlay())); 
-  console.log(playRound("scissors", computerPlay())); 
-  */
+  let numRounds;
 
-game();
+  while (true) {
+    numRounds = prompt("How many rounds do you want to play?");
+  
+    if (isNaN(numRounds)) {
+      console.log("Please enter a valid number.");
+    } else {
+      break;
+    }
+  }
+  
+  game(numRounds);
